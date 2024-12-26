dc31f17911addff62bda40658ec39d5741b9c9f0
i-anonymous03 <81768571+i-anonymous03@users.noreply.github.com>
2024-12-26 14:56:57 +0530

    Create Main.java
    exception handling in java

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+    int a=10,b=0;
+
+         void set()
+        {
+                 try{
+            int data1 =a/b;
+            System.out.println(data1);
+        }
+    
+     catch(ArithmeticException e){
+        
+        System.out.println("number cant be divisible by 0");}
+        }
+        void rop(){
+            int data2 =a*b;
+        System.out.println(data2 +"\n rest of program");
+        System.out.println("\nend of program");
+        }
+    public static void main(String[] args) {
+
+     Main m1=new Main();
+        m1.set();
+        m1.rop();
+    }
+}
