13473f4bd45956b18c9c75fccee8c0314466c261
i-anonymous03 <81768571+i-anonymous03@users.noreply.github.com>
2025-01-07 12:42:24 +0530

    NullPointerExceptionExample.java

--- NullPointerExceptionExample.java
+++ NullPointerExceptionExample.java
@@ -9,32 +9,3 @@ System.out.println("Error: Null reference encountered.");
 }
 }
 }
-
-NumberFormatException
-
-public class NumberFormatExceptionExample {
-public static void main(String[] args) {
-String str = "abc";
-try {
-int num = Integer.parseInt(str);
-System.out.println("Parsed number: " + num);
-} catch (NumberFormatException e) {
-System.out.println("Error: Unable to parse the string as an integer.");
-}
-}
-}
-
-ArrayIndexOutOfBoundsException
-
-public class ArrayIndexOutOfBoundsExceptionExample {
-public static void main(String[] args) {
-int[] numbers = {1, 2, 3, 4, 5};
-try {
-int index = 10; // Accessing an index that is out of bounds
-int value = numbers[index];
-System.out.println("Value at index " + index + ": " + value);
-} catch (ArrayIndexOutOfBoundsException e) {
-System.out.println("Error: Index is out of bounds.");
-}
-}
-}
